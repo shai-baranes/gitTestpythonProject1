aab9eae985886c9fa8521d2ff217537ff1228e3a
shai-baranes <129833515+shai-baranes@users.noreply.github.com>
2023-04-04 10:21:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm') # comment
+    print_hi('PyCharm') # comment - and now updated
 
 
 
